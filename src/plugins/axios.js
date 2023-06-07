94440141d22dc7a61a406ad53e91d64a5464bfc7
Riska <riskaputrianggraini@upi.edu>
2023-06-08 00:15:30 +0700

    integrasi api

---
+++ src/plugins/axios.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { getCookies, delCookies } from './cookies';
+
+//Restful API config
+axios.defaults.headers['Content-Type'] = 'aplication/json'
+
+//Endpoint
+const hostname = import.meta.env.VITE_BASE_API_URL;
+
+//Instance Creation
+const baseApi = axios.create({
+    baseURL: hostname,
+});
+
+//Request Config
+baseApi.interceptors.request.use(
+    (config) => {
+        const token = getCookies('CERT');
+        if (token) config.headers['Authorization'] = 'Bearer ${token';
+        else {
+            delCookies('CERT');
+            delete config.headers['Authorization'];
+        }
+
+        return config;
+    },
+    (error) => {
+        throw error;
+    },
+);
+
+//Response Config
+baseApi.interceptors.response.use(
+    (response) => response.data,
+    (error) => {
+        switch (error.response.status) {
+            case 401:
+                delCookies('CERT')
+                break;
+                default:
+                    break;
+        }
+        throw error?.response?.console.error ?? console.error?.response?.message ?? error;
+    },
+)
+
+export { baseApi };
